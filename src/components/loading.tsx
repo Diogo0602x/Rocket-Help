c64abaa053c1dc3292835f818bb241dffb0da203
Diogo Sales Teixeira <diogoteixeira0602@gmail.com>
2022-07-18 15:49:11 -0300

    Aula 2 - terminada

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -2,8 +2,8 @@ import { Center, Spinner } from 'native-base';
 
 export function Loading() {
   return(
-    <Center flex={1} bg="gray.600">
-      <Spinner color="gray.100" />
+    <Center flex={1} bg="gray.700">
+      <Spinner color="secondary.700" />
     </Center>
   );
 }
--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Heading, Icon, useTheme, VStack } from "native-base";
 import { Envelope, Key } from 'phosphor-react-native';
 
@@ -7,7 +8,14 @@ import { Input } from "../components/Input";
 import { Button } from "../components/Button";
 
 export function SignIn() {
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
+
   const { colors } = useTheme();
+
+  function handleSignIn() {
+    console.log(name, password)
+  }
 
   return (
     <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
@@ -20,15 +28,17 @@ export function SignIn() {
         placeholder="E-mail" 
         mb={4}
         InputLeftElement={<Icon as={<Envelope color={colors.gray[300]} />} ml={4} />}
+        onChangeText={setName}
       />
       <Input 
         mb={8}
         placeholder="Senha" 
         InputLeftElement={<Icon as={<Key color={colors.gray[300]}/>} ml={4} />}
         secureTextEntry
+        onChangeText={setPassword}
       />
 
-      <Button title="Entrar" w="full"  />
+      <Button title="Entrar" w="full" onPress={handleSignIn}  />
     </VStack>
   );
 }
