cd07a4a3de09a89f2a8e365764dc1ee400b5e4e6
Diogo Sales Teixeira <diogoteixeira0602@gmail.com>
2022-07-19 14:31:58 -0300

    Register done

--- App.tsx
+++ App.tsx
@@ -3,8 +3,8 @@ import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/
 
 import { THEME } from './src/styles/theme';
 
-import { Home } from './src/screens/Home';
-import { Loading } from './src/components/loading';
+import { Register } from './src/screens/Register';
+import { Loading } from './src/components/Loading';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold});
@@ -16,7 +16,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <Home/> : <Loading/>}
+      {fontsLoaded ? <Register/> : <Loading/>}
     </NativeBaseProvider>
   );
 }
